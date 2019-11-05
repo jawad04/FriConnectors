f84e372fbde63745bf69b18665a10003c8cda73a
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-04 22:22:39 -0500

    modified users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check, valiadtionResult } = require("express-validator");
+const { check, validationResult } = require("express-validator/check");
 
 // @route POST api/users
 // @description Register User
@@ -10,10 +10,18 @@ router.post(
   [
     check("name", "Name is required")
       .not()
-      .isEmpty()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
   ],
   (req, res) => {
-    console.log(req);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send("User route");
   }
 );
