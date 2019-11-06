8ebe2815c6742740182b603083e7870ed94152ff
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-05 23:09:45 -0500

    added jsonwebtoken for user id

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route POST api/users
 // @description Register User
@@ -51,7 +53,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
       // return jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
