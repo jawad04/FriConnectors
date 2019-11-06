888794d70df3118790680bce3c90c85607e08e61
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-05 23:54:03 -0500

    added middleware to validate token

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // check if there is no token
+  if (!token) {
+    return res.sendStatus(401).json({ msg: "No token, authorization failed" });
+  }
+  //verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 // @route GET api/auth
 // @description Test route
 //@access public
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500).send("Server error");
+  }
 });
 
 module.exports = router;
