ab408df19001e350a172367eef49568b4e642c55
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-06 22:36:20 -0500

    added route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,4 +107,41 @@ router.post(
     }
   }
 );
+
+// @route         GET api/profile
+// @description   Get all profiles
+//@access         Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route         GET api/profile/user/:user_id
+// @description   Get profile by userId
+//@access         Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    if (error.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
