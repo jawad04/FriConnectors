85f69c42d760e6232258ac12c647b441691e4986
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-07 22:46:05 -0500

    makes request to get github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
@@ -322,6 +324,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     } else {
       res.status(400).json({ msg: "id doesn't exits" });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route         GET api/profile/github/:username
+// @description   Get user repos from Github
+//@access         Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js " }
+    };
+    request(options, (error, response, body) => {
+      if (error) console.log(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("Server error");
