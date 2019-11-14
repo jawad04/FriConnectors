1a41b88660e71e99396b7fb0261ce1ddcec8d168
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-11-13 22:10:01 -0500

    added logout action to log users out

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -80,4 +81,12 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout/ Clear Profile
+
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
